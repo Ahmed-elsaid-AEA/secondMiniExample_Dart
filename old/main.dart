2023297b30d82abbbc4ff6790834a648f2217ad1
ahmed-abmouty <ahmed122727727@gmail.com>
2023-09-28 19:34:21 +0300

    uplodaing my project

---
+++ old/main.dart
@@ -0,0 +1,21 @@
+void main() {
+  /**
+   * permitive
+   * int  -> number 1 2 3
+   * double -> 1 2 3 1.5 1.6
+   * bool  -> true / false
+   * String  -> text
+   * dynamic -> number and text ...........
+   */
+  // double x;
+  // x = 5.5;
+  int $_$a1ge1_98566585_$ = 20;
+  String name = "ad";
+  String firstNameOfAhmed = "ahmed";
+  //camel case
+  //kebab case
+  //sanck case
+  //can't start with number
+  //can't put special charcter
+  // can put _  $
+}
